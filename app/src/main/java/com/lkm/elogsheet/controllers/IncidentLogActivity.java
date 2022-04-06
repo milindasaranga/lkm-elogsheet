87ca1176fd6249c18a5acba0895d84cbaf873bb4
“milindadev” <msck.dev@gmail>
2022-04-06 23:14:21 +0400

    Incident capture

---
+++ app/src/main/java/com/lkm/elogsheet/controllers/IncidentLogActivity.java
@@ -0,0 +1,16 @@
+package com.lkm.elogsheet.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lkm.elogsheet.R;
+
+public class IncidentLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incident_log);
+    }
+}
